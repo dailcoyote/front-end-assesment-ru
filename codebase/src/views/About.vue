261e5dcd2bb5ca9580647cdf19addecd5bc30b57
dailcoyote <dailcoyote@gmail.com>
2020-08-31 13:14:15 +0600

    +About.vue

--- codebase/src/views/About.vue
+++ codebase/src/views/About.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>About</div>
+  <div id="about">
+    <h2>What Makes a Great About Us Page?</h2>
+    <br>
+    <p>Your About Us page should be:</p>
+    <ul>
+      <li>Informative. It doesn&rsquo;t always have to tell your whole story, but it should at least provide people with an idea of .</li>
+      <li>Contain social proof, testimonials, and some personal information that visitors can relate to, such as education, family, etc.</li>
+      <li>Useful.</li>
+      <li>Engaging.</li>
+      <li>Easy to navigate.</li>
+      <li>Accessible on any device.</li>
+    </ul>
+    <br>
+    <p>Does that all sound complicated?</p>
+    <p>The main purpose of your About Us page is to give your visitors a glimpse into who you are as a person or a business (or sometimes both).</p>
+    <p>As users discover your brand, they need to distinguish what sets you apart and makes you&hellip; you.</p>
+    <p>This often requires finding the right balance between compelling content and a design carefully planned to look the part.</p>
+    <p>Conveying your identity in a fun and approachable &ndash; but also reliable and informative &ndash; way is challenging.</p>
+    <p>If you know who you are and your goal for your site, the About Us page should come naturally.</p>
+    <p>However, if you&rsquo;re looking for some inspiration, you can always check out the following 25 awesome examples of About Us pages.</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "About",
+  name: "About"
 };
 </script>
 
-<style>
+<style scoped>
+#about {
+  width: 65%;
+  margin-left: 25%;
+  margin-right: 10%;
+}
 </style>
